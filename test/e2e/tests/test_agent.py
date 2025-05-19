a6eb91500f7194c22fe0426dbe0f1d4f90a11211
knottnt <trevor.knottnerus@gmail.com>
2025-05-18 20:16:09 -0700

    Update test_agent.py
    - Fix bad inference profile region validation error.

--- test/e2e/tests/test_agent.py
+++ test/e2e/tests/test_agent.py
@@ -40,7 +40,7 @@ def simple_agent():
     agent_name = random_suffix_name("bedrock-test-agent", 32)
     agent_description = "Test agent for e2e testing"
     agent_instruction = "You are a helpful assistant that provides information about AWS services."
-    agent_model = "arn:aws:bedrock:us-east-2:807147659905:inference-profile/us.amazon.nova-lite-v1:0"
+    agent_model = "arn:aws:bedrock:us-west-2:807147659905:inference-profile/us.amazon.nova-lite-v1:0"
     agent_role_arn = get_bootstrap_resources().AgentRole.arn
     agent_prompt_temp = "0.7"
     agent_top_p = "0.9"
