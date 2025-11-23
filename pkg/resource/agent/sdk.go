6a55d1573f0670b180f727c90441974d2ba8dddc
ci-robot <ci-robot@k8s.io>
2025-11-23 09:13:39 +0000

    Update to ACK runtime `v0.54.1`, code-generator `v0.54.0`

--- pkg/resource/agent/sdk.go
+++ pkg/resource/agent/sdk.go
@@ -638,7 +638,7 @@ func (rm *resourceManager) newCreateRequestPayload(
 	if r.ko.Spec.IdleSessionTTLInSeconds != nil {
 		idleSessionTTLInSecondsCopy0 := *r.ko.Spec.IdleSessionTTLInSeconds
 		if idleSessionTTLInSecondsCopy0 > math.MaxInt32 || idleSessionTTLInSecondsCopy0 < math.MinInt32 {
-			return nil, fmt.Errorf("error: field idleSessionTTLInSeconds is of type int32")
+			return nil, fmt.Errorf("error: field IdleSessionTTLInSeconds is of type int32")
 		}
 		idleSessionTTLInSecondsCopy := int32(idleSessionTTLInSecondsCopy0)
 		res.IdleSessionTTLInSeconds = &idleSessionTTLInSecondsCopy
